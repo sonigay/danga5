5baab4c8205224b4f405effa276730492406ac8c
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-18 01:28:27 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ import random
 import os
 import datetime
 import gspread
+from discord.ext import commands
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -17,6 +18,7 @@ sheet1 = doc.worksheet('무선출력')
 client = discord.Client()
 
 
+
 @client.event
 async def on_ready():
     print("login")
@@ -26,10 +28,33 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name='주문재고 전달', type=1))
 
 
+bot = commands.Bot(command_prefix='!')
 
+async def func_comeon(ctx):
+    voice = bot.voice_client_in(ctx.message.server)
+    if ctx.message.author.voice.voice_channel:
+        if voice:
+            if voice.channel != ctx.message.author.voice.voice_channel:
+                print('move to channel')
+                await voice.move_to(ctx.message.author.voice.voice_channel)
+        else:
+            print('join to channel')
+            voice = await bot.join_voice_channel(ctx.message.author.voice.voice_channel)
+    else:
+        await bot.say('where are you?')
+        print('stay...')
+    return voice
 
+@bot.command(pass_context=True)
+async def comeon(ctx):
+    await func_comeon(ctx)
 
-
-                        
+@bot.command(pass_context=True)    
+async def play(ctx):
+    voice = await func_comeon(ctx)
+    player = voice.create_ffmpeg_player('music.mp3')
+    player.start()
+    
+    
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
