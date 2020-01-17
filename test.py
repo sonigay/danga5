cbfb1689c371a917f12a5aaef5252b8ef0ee918d
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-18 01:18:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds']
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('jungsanfile-e5ae2dbc8879.json', scope)
 client = gspread.authorize(creds)
-doc = client.open_by_url('https://docs.google.com/spreadsheets/d/15p6G4jXmHw7Z_iRCYeFwRzkzLxqf-3Pj0c6FeVuFYBM/edit#gid=0')
+doc = client.open_by_url('https://docs.google.com/spreadsheets/d/1hL4uvq2On11zp-_JWoWMG0Gyyuty5Lhvp_gQkfTYsOI')
 
-sheet1 = doc.worksheet('재고주문')
+sheet1 = doc.worksheet('무선출력')
 
 
 client = discord.Client()
@@ -28,22 +28,7 @@ async def on_ready():
 
 
 
-@client.event
-async def on_message(message):
-    
-    if message.content.startswith('!주문'):
-        if message.channel.is_private and message.author.id != "538289410018639893":
-            await client.send_message(discord.utils.get(client.get_all_members(), id="315237238940106754"), message.author.name + "(" + message.author.id + ") : " + message.content[4:] + " 주문이 완료 되었습니다. 부득이 한경우 개인답변 드리도록 하겠습니다.")
-                        
-    if message.content.startswith('!주문'):
-        if message.channel.is_private and message.author.id != "538289410018639893":
-            sheet1.insert_row([message.author.display_name, message.content[4:]], 3)
-            await client.send_message(client.get_channel("661768769131249667"), message.author.display_name + "(" + message.author.id + ") : " + message.content[4:])
-           
-            
-    if message.content.startswith("!답변"):
-            member = discord.utils.get(client.get_all_members(), id=message.content[4:22])
-            await client.send_message(member, "홍팀장 개인답변 : " + message.content[23:])
+
 
                         
 access_token = os.environ["BOT_TOKEN"]
