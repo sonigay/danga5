56bf2b1f9a304487936edb1da9407c2d4e8d2cf9
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-20 20:33:41 +0900

    Update test.py

--- test.py
+++ test.py
@@ -49,7 +49,7 @@ async def on_message(message):
         command_list += '!동판 단독\n'     #!동판
         command_list += '!주문 [주문넣을 단말기및 요청글] 채널:재고신청봇 개인메시지\n'     #!동판
         command_list += '!단가 [모델명 요금제군 유형] 채널:무선정책조회\n'     #!동판
-        command_list += '!단가 [모델명 요금제군 유형] 채널:외국인정책조회\n'     #!동판
+        command_list += '```fix\n!단가 [모델명 요금제군 유형] 채널:외국인정책조회\n```\n'     #!동판
         
         
         embed = discord.Embed(
@@ -59,7 +59,7 @@ async def on_message(message):
             )
         embed.add_field(
             name=":radio: 업무외지원 명령어 ",
-            value= '```fix!주사위\n!복권\n```'
+            value= '```!주사위\n!복권\n```'
             )
         await client.send_message(message.channel, embed=embed)
     
