1c714bf52579768c679f4b40923edda6c3c65d3e
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-01 15:07:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -38,19 +38,15 @@ async def on_message(message):
         command_list += '!재고 모델명\n'     #!재고+모델명
         command_list += '!재고 [구단위]\n'     #!재고+구단위
         command_list += '!퀵비 [동단위/동단위]\n'     #!퀵비
-        command_list += '!동판 동판신규\n'     #!동판
-        command_list += '!동판 동판기변\n'     #!동판
-        command_list += '!동판 소호신규\n'     #!동판
-        command_list += '!동판 소호기변\n'     #!동판
+        command_list += '!동판 동판\n'     #!동판
+        command_list += '!동판 소호\n'     #!동판
         command_list += '!동판 후결합\n'     #!동판
-        command_list += '!동판 재약정기존\n'     #!동판
-        command_list += '!동판 재약정전환\n'     #!동판
-        command_list += '!동판 재약정단독기존\n'     #!동판
-        command_list += '!동판 재약정단독전환\n'     #!동판
+        command_list += '!동판 재약정\n'     #!동판
+        command_list += '!동판 재약정단독\n'     #!동판
         command_list += '!동판 단독\n'     #!동판
-        command_list += '!주문 [주문넣을 단말기]\n'     #!주문
-        command_list += '!단가 [모델명 요금제군 유형] 채널:무선정책조회\n'     #!동판
-        command_list += '!단가 [모델명 요금제군 유형] 채널:외국인정책조회\n'     #!동판
+        command_list += '!주문 ex)!주문 N976 화이트 1대 보내주세요\n'     #!주문
+        command_list += '!단가 ex)!단가 N976 A군 MNP 채널:무선정책조회\n'     #!동판
+        command_list += '!단가 ex)!단가 N976 A군 MNP 채널:외국인정책조회\n'     #!동판
         
         
         embed = discord.Embed(
