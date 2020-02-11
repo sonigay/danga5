8c50af5419669469a47823e936e0e275fe80c94f
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-12 01:29:26 +0900

    Update test.py

--- test.py
+++ test.py
@@ -35,14 +35,6 @@ async def on_message(message):
         command_list = ''
         command_list += '!명령어\n'     #!명령어        
         command_list += '!모델명\n'     #!모델명
-        command_list += '!동판 동판\n'     #!동판
-        command_list += '!동판 소호신규\n'     #!동판
-        command_list += '!동판 소호기변\n'     #!동판
-        command_list += '!동판 후결합\n'     #!동판
-        command_list += '!동판 재약정\n'     #!동판
-        command_list += '!동판 재약정단독\n'     #!동판
-        command_list += '!동판 단독\n'     #!동판
-        command_list += '\n'
         command_list += '!주문 주문내용\n'     #!주문
         command_list += 'ex)!주문 N976 화이트 1대 보내주세요\n'     #!주문
         command_list += '\n'
@@ -55,17 +47,17 @@ async def on_message(message):
             )
         embed.add_field(
             name=":radio: ❗정책관련 명령어 ",
-            value= '```diff\n=!정책표\n=!그레이드\n+!단가 모델명 요금제군 유형\n-ex)!단가 N976 A군 MNP\n+!외국인단가 모델명 요금제군 유형\n-ex)!외국인단가 N976 A군 MNP\n+!공짜폰 요금제군 유형\n-ex)!공짜폰 C군 MNP\n+!외국인공짜폰 요금제군 유형\n-ex)!외국인공짜폰 A군 신규\n ```',
+            value= '```diff\n=- !정책표\n=- !그레이드\n+ !단가 모델명 요금제군 유형\n---< ex)!단가 N976 A군 MNP >\n+ !외국인단가 모델명 요금제군 유형\n---< ex)!외국인단가 N976 A군 MNP >\n+ !공짜폰 요금제군 유형\n---< ex)!공짜폰 C군 MNP >\n+ !외국인공짜폰 요금제군 유형\n---< ex)!외국인공짜폰 A군 신규 >\n ```',
             inline = False            
             )
         embed.add_field(
             name=":radio: ❗재고관련 명령어 ",
-            value= '```diff\n+!재고 모델명\n-ex)!재고 N976\n+!재고 [구단위]\n-ex)!재고 남동구\n+!퀵비 [동단위/동단위]\n-ex)!퀵비 논현동/가좌동\n---퀵비 멍령어는 실행은 되지만\n데이터량이 많아 다소 결과가 늦게 나옴\n ```',
+            value= '```diff\n- !주문\n+ !재고 모델명\n---< ex)!재고 N976 >\n+ !재고 [구단위]\n---< ex)!재고 남동구 >\n+ !퀵비 [동단위/동단위]\n---< ex)!퀵비 논현동/가좌동 >\n퀵비 멍령어는 실행은 되지만\n데이터량이 많아 다소 결과가 늦게 나옴\n ```',
             inline = False            
             )
         embed.add_field(
             name=":radio: ❗동판관련 명령어 ",
-            value= '```!영화순위\n\n!주사위\n\n!복권\n\n!나이 생년-월-일 \nex)!나이 2002-02-01\n\n!유지기간 개통일 \nex)!유지기간 2020-01-01\n\n!사다리 뽑을인원수 인원1 인원2 인원3...\nex)!사다리 2 홍길동 갑돌이 갑순이\n```',
+            value= '```tex\n$ !동판 동판\n& !동판 소호신규\n& !동판 소호기변\n& !동판 후결합\n& !동판 재약정\n& !동판 재약정단독\n& !동판 단독\n ```',
             inline = True            
             )
         embed.add_field(
