4a877735e7a8e8e5e692ec93fed0b8a97ec031a3
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-12 01:40:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -59,7 +59,7 @@ async def on_message(message):
             )
         embed.add_field(
             name=":radio: ❗기타 명령어 ",
-            value= '```diff\n= !영화순위\n\n= !주사위\n\n= !복권\n\n+ !나이 생년-월-일 \n---< ex)!나이 2002-02-01 >\n+ !유지기간 개통일\n---< ex)!유지기간 2020-01-01 >\n+ !사다리 뽑을인원수 인원1 인원2 인원3...\n---< ex)!사다리 2 홍길동 갑돌이 갑순이 >```',
+            value= '```diff\n= !영화순위\n= !주사위\n= !복권\n+ !나이 생년-월-일 \n---< ex)!나이 2002-02-01 >\n+ !유지기간 개통일\n---< ex)!유지기간 2020-01-01 >\n+ !사다리 뽑을인원수 인원1 인원2 인원3...\n---< ex)!사다리 2 홍길동 갑돌이 갑순이 >```',
             inline = True            
             )        
         await client.send_message(message.channel, embed=embed)
