a73e7c9d5d6ab508bb500d827d8cb423babe0129
sonigay <43840048+sonigay@users.noreply.github.com>
2020-02-12 00:22:34 +0900

    Update test.py

--- test.py
+++ test.py
@@ -81,7 +81,7 @@ async def on_message(message):
             )
         embed.add_field(
             name=":radio: ❗업무외지원 명령어 ",
-            value= '```!영화순위\n\n!주사위\n\n!복권\n\n!나이 생년-월-일 fix\nex)!나이 2002-02-01\n\n!유지기간 개통일 fix\nex)!유지기간 2020-01-01\n\n!사다리 뽑을인원수 인원1 인원2 인원3...fix\nex)!사다리 2 홍길동 갑돌이 갑순이\n```'
+            value= '```!영화순위\n\n!주사위\n\n!복권\n\n!나이 생년-월-일 fix\nex)!나이 2002-02-01\n\n!유지기간 개통일 fix\nex)!유지기간 2020-01-01\n\n!사다리 뽑을인원수 인원1 인원2 인원3...fix\nex)!사다리 2 홍길동 갑돌이 갑순이\n```',
             inline = False            
             )
         await client.send_message(message.channel, embed=embed)
